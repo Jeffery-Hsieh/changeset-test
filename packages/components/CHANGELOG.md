74a8d61a68ca9dbcc17b339ce2e699ccbb2b4985
Jeffery-Hsieh <jefferyhsieh1996@gmail.com>
2022-06-09 15:31:12 +0800

    initial commit

---
+++ packages/components/CHANGELOG.md
@@ -0,0 +1,7 @@
+# @jeffery-hsieh/components
+
+## 2.0.0
+
+### Major Changes
+
+- Move button component to saperate file
---
+++ packages/components/src/button.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const Button = ({ onClick, children, isSelected }) => (
+  <button
+    style={{
+      border: 0,
+      backgroundColor: isSelected ? "rebeccapurple" : "hotpink",
+      color: isSelected ? "white" : "black",
+      padding: "12px 24px",
+      margin: "12px",
+      borderRadius: "3px",
+    }}
+    onClick={onClick}
+  >
+    {children}
+  </button>
+);
